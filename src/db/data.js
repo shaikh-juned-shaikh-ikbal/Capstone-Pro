89e4f4ff6eb77954149212c06e0292f45b4d6185
Shaikh juned <sheikhjuned770@gmail.com>
2023-09-14 13:07:31 +0530

    databas

--- src/db/data.js
+++ src/db/data.js
@@ -5,7 +5,7 @@ const data = [
     img: "https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg",
     title: "Nike Air Monarch IV",
     star: <AiFillStar className="rating-star" />,
-    reviews: "(123 reviews)",
+    reviews: "(123 reviews)", 
     prevPrice: "$140,00",
     newPrice: "200",
     company: "Nike",
